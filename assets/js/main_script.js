da48cdd44dae7c029e7320e3c3dcebc6d04e7915
Lou Stringer <louise.stringer@gmail.com>
2018-02-27 15:27:15 +0000

    add details of this site to work section

--- assets/js/main_script.js
+++ assets/js/main_script.js
@@ -41,6 +41,7 @@ setInterval(function() {
 var workExamples = document.querySelectorAll(".work-example");
 var workDetail = document.querySelectorAll(".work-example > .work-detail")
 
+
 // write function for workClicked/workTabbed
 
 for (var i = 0; i < workExamples.length; i++) {
@@ -51,34 +52,28 @@ for (var i = 0; i < workExamples.length; i++) {
 
 function workClicked() {
  for (var i = 0; i < workExamples.length; i++) {
-   // workExamples[i].classList.add("work-example");
-   // workExamples[i].classList.remove("work-example-clicked");
    workDetail[i].classList.add("hide");
    if (workExamples[i] === this) {
-     // this.classList.remove("work-example");
-     // this.classList.add("work-example-clicked");
      workDetail[i].classList.remove("hide");
      window.scrollTo(0, this.offsetTop - 70);
    };
  };
 };
 
-// function workClicked() {
-//  for (var i = 0; i < workExamples.length; i++) {
-//    workExamples[i].classList.add("work-example");
-//    workExamples[i].classList.remove("work-example-clicked");
-//    workDetail[i].classList.add("hide");
-//    if (workExamples[i] === this) {
-//      this.classList.remove("work-example");
-//      this.classList.add("work-example-clicked");
-//      workDetail[i].classList.remove("hide");
-//      window.scrollTo(0, this.offsetTop - 70);
-//    };
-//  };
-// };
-
 function workTabbed(event) {
   if (event.keyCode === 13) {
-    workClicked();
+    for (var i = 0; i < workExamples.length; i++) {
+      workDetail[i].classList.add("hide");
+      if (workExamples[i] === this) {
+        workDetail[i].classList.remove("hide");
+        window.scrollTo(0, this.offsetTop - 70);
+      };
+    };
   };
 }
+
+// function workTabbed(event) {
+//   if (event.keyCode === 13) {
+//     workClicked();
+//   };
+// }
